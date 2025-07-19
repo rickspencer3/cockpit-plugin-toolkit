36ae024696da27ea0471d30c0665d20cec4bf2ab
Rick Spencer <richard.linger.spencer.3@gmail.com>
2025-07-19 15:58:54 -0400

    removed garbage comments

--- template-plugin/script.js
+++ template-plugin/script.js
@@ -1,5 +1,4 @@
 
-// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", () => {
     console.log("My Cool Plugin: DOMContentLoaded");
     console.log("Tesing the cockpit API.");
@@ -14,11 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error("Command failed", err);
     });
 
-    // Get the button and message area elements
     const myButton = document.getElementById("myButton");
     const messageArea = document.getElementById("messageArea");
 
-    // Add a click event listener to run a command on the server
     if (myButton) {
         myButton.addEventListener("click", () => {
             messageArea.textContent = "Button worked";
